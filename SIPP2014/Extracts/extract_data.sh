8bfa193f8125de2d7a861eb0180d9c02a62e29f2
Robert Reynolds <rreynolds@prc.utexas.edu>
2017-11-06 10:38:29 -0600

    Change the default data source to be the zip file in our shared data structure.
    For now we will unzip it every time, but leave the result lying here.

--- SIPP2014/Extracts/extract_data.sh
+++ SIPP2014/Extracts/extract_data.sh
@@ -7,7 +7,11 @@ then
     echo "   Optionoally, provide a second parameter specifying the"
     echo "   location of the SIPP 2014 data file."
     echo "   In the absernce of a second parameter the location"
-    echo "   defaults to ./pu2014w1.dat"
+    echo "   defaults to the zip file in our shared data directory,"
+    echo "   which means this script will unzip it first."
+    echo ""
+    echo "   At present, the unzipped file is left in this directory."
+    echo "   So don't forget to delete it if you don't want it eating your disl space."
     echo ""
     echo "   The format of the <field_list_file> should be"
     echo "   one variable per line, with the variable name first"
@@ -30,6 +34,7 @@ if [ $# -gt 1 ]
 then
     datafile=$2
 else
+    unzip "/Users/Robert/Box Sync/SIPP/data/SIPP2014/pu2014w1_dat.zip"
     datafile=./pu2014w1.dat
 fi
 
